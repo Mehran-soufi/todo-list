17d9029172eaaa5dcaeb1f97cf59274f1f905550
mehran-soufi <mehransoufi33@gmail.com>
2023-05-02 15:10:18 +0430

    project scale

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import './style.css'
+
+const Main = () => {
+    return ( 
+        <Container className="main">
+            <p>Hello world</p>
+        </Container>
+     );
+}
+ 
+export default Main;
